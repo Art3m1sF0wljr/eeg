65e5df8940dc61d97be1d2b9a271a9d93d9b773a
Daniel@thinkpad <s319884@studenti.polito.it>
2025-03-24 16:04:48 +0100

    only data and time

--- full_spatiotemporal/def_full_spatiotemporal_full_laplacian.m
+++ full_spatiotemporal/def_full_spatiotemporal_full_laplacian.m
@@ -74,17 +74,17 @@ rho = 1.0;      % ADMM penalty parameter
 dipole_positions = DipoleField.pos(good_dipoles, :); % N_dipoles x 3 matrix
 sigma = 10; % Scaling parameter
 neighborhood_threshold = 10; 
-verbose = 2;  %0,1,2
+verbose = 1;  %0,1,2
 L_s = construct_spatial_laplacian(dipole_positions, sigma, neighborhood_threshold);
 
 % Temporal smoothness matrix (T x T)
 L_t = diag(ones(T-1,1), 1) + diag(ones(T-1,1), -1) - 2*eye(T); % Temporal smoothness matrix, approximates second derivative thru finite differences
 
 % Solve the inverse problem
-J_reconstructed = solve_inverse_problem(B, A, L_s, L_t, lambda_s, lambda_t, T, tol, max_iter);
+%J_reconstructed = solve_inverse_problem(B, A, L_s, L_t, lambda_s, lambda_t, T, tol, max_iter);
 %J_reconstructed = ADMM_L1_minimization(B, A, L_s, L_t, lambda_s, lambda_t, rho, max_iter, tol, verbose)
 %J_reconstructed = ADMM_L1_minimization_GPU(B, A, L_s, L_t, lambda_s, lambda_t, rho, max_iter, tol, verbose)
-
+J_reconstructed = solve_inverse_L1_L1(B, A, L_t, lambda_t, rho, max_iter, tol)
 % Display results
 % Plot for the inverse problem
 figure;
